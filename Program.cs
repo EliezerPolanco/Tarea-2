80553f24b5da7f706eebdeb03df4a6315573fa1c
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-08-03 23:12:50 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+        Console.WriteLine("Ingrese 10 números desordenados:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(numeros);
+
+        Console.WriteLine("Números ordenados:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
